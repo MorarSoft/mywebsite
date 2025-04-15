561554a92ab2cdae234fe0053b522a48eddbeae0
Ax Mo <axom1115@gmail.com>
2025-04-15 01:39:46 +0000

    feat: set output to public for vercel

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: 'public',
 };
 
 export default nextConfig;
